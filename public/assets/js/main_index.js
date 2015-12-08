03633d91eacf916e7e04c4636025eb49de460e94
redwoodxiao <xiaoredwood@gmail.com>
2015-12-08 02:54:33 -0800

    fixed http importing script for heroku, implemented pagenation on front page

--- public/assets/js/main_index.js
+++ public/assets/js/main_index.js
@@ -31,7 +31,30 @@
 
     mainIndexApp.controller('loadCoursesController', ['$scope', '$http', '$location', '$window', function ($scope, $http, $location, $window) {
         $http.get("/api/courses").then(function (response) {
-            $scope.listOfCourses = response.data
+            console.log("how many times is this called");
+            $scope.listOfCourses = response.data;
+            var allNumberOfItems = $scope.listOfCourses.length;
+            $scope.totalItems = allNumberOfItems;
+            $scope.currentPage = 1;
+            $scope.itemsPerPage = 4;
+
+
+            $scope.coursesShowChange = function() {
+                console.log("courses changed : " + $scope.listOfCourses.length);
+            }
+
+
+            $scope.setPage = function (pageNo) {
+                $scope.currentPage = pageNo;
+                console.log('Set Page to:' + pageNo);
+
+
+            };
+            $scope.pageChanged = function() {
+                console.log('Page changed toxxxx : ' + $scope.currentPage + ' ' + $scope.courseDatas.length);
+
+            };
+
         });
 
     }]);
